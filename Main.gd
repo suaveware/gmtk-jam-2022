16fb53f47421608c8398a502da2f9f1c6477b98d
Luiz de Oliveira <luiz@noredink.com>
2022-07-15 19:45:18 -0400

    just for the matrix but goes everything

--- Main.gd
+++ Main.gd
@@ -1,5 +1,14 @@
 extends Spatial
 
-
 func _ready() -> void:
 	pass
+
+func _physics_process(_delta):
+	if Input.is_action_pressed("roll_up"):
+		$Player.roll(Vector3.FORWARD)
+	if Input.is_action_pressed("roll_down"):
+		$Player.roll(Vector3.BACK)
+	if Input.is_action_pressed("roll_right"):
+		$Player.roll(Vector3.RIGHT)
+	if Input.is_action_pressed("roll_left"):
+		$Player.roll(Vector3.LEFT)
