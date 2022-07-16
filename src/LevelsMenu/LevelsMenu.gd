7d3e4b0cf4a41b0c11b17be346a97330987b1095
Eric Lopes <erichhlopes@gmail.com>
2022-07-16 18:27:58 -0400

    level selector

---
+++ src/LevelsMenu/LevelsMenu.gd
@@ -0,0 +1,30 @@
+extends Control
+
+var level_quantity = 9
+
+var LevelButton = preload("res://src/LevelsMenu/LevelButton.tscn")
+onready var button_grid = $GridContainer
+
+
+func _ready():
+	var tutorialButton = LevelButton.instance()
+	tutorialButton.text = 'Tutorial'
+	button_grid.add_child(tutorialButton)
+	tutorialButton.connect("button_up", self, "_on_tutorialButton_pressed")
+	
+	for i in range(0, level_quantity):
+		var button = LevelButton.instance()
+		button.text = 'Level '+((i+1) as String)
+		button_grid.add_child(button)
+		button.connect("button_up", self, "_on_button_pressed", [i])
+		
+
+func _on_button_pressed(level):
+	GlobalState.level_index = level
+	get_tree().change_scene("res://Main.tscn")
+	
+func _on_tutorialButton_pressed():
+	get_tree().change_scene("res://src/tutorial/Tutorial.tscn")
+
+func _on_MenuButton_pressed():
+	get_tree().change_scene("res://src/StartMenu/StartMenu.tscn")
--- src/StartMenu/StartMenu.gd
+++ src/StartMenu/StartMenu.gd
@@ -9,6 +9,9 @@ func _on_StartButton_pressed() -> void:
 	get_tree().change_scene("res://src/tutorial/Tutorial.tscn")
 	pass
 
+func _on_LevelsButton_pressed() -> void:
+	get_tree().change_scene("res://src/LevelsMenu/LevelsMenu.tscn")
+	pass
 
 func _on_QuitButton_pressed() -> void:
 	get_tree().quit()
