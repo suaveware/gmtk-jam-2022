443bb97540f46705ec5249f47120a97dbefce8c8
Luiz de Oliveira <luiz@noredink.com>
2022-07-16 08:49:56 -0400

    avoid holes when moving instead of edge of a square grid

--- src/classes/Grid.gd
+++ src/classes/Grid.gd
@@ -27,6 +27,7 @@ func _ready() -> void:
 
 func load_level(level: Level = null):
 	for child in get_children():
+		positions = {}
 		child.queue_free()
 
 	if(level):
@@ -36,7 +37,7 @@ func load_level(level: Level = null):
 		player.translation = Vector3(level.StartPosition.x* tile_size,1,level.StartPosition.y* tile_size) + translation
 	for i in range(0, width):
 		for j in range(0, height):
-			if(!positions[Vector2(i, j)]):
+			if(!positions.get(Vector2(i, j))):
 				instantiate_tile(i, j, rand_range(1,7))
 
 func instantiate_tile(x: int, y: int, value: int):
@@ -98,20 +99,26 @@ func get_facing_down_direction() -> Position3D:
 	return lowest
 
 
-func player_can_roll(position: Vector3):
+func player_can_roll(direction: Vector3):
 	if owner.has_won:
 		return false
 
-	match(position):
-		Vector3.FORWARD:
-			return player_grid_position.y > 0
-		Vector3.BACK:
-			return player_grid_position.y < height-1
-		Vector3.RIGHT:
-			return player_grid_position.x < width-1
-		Vector3.LEFT:
-			return player_grid_position.x > 0
-	return false
+	var target_grid_position = player_grid_position + Vector2(direction.x, direction.z)
+
+	return not not positions.get(target_grid_position)
+
+
+#	match(direction):
+#		Vector3.FORWARD:
+#
+#			return player_grid_position.y > 0
+#		Vector3.BACK:
+#			return player_grid_position.y < height-1
+#		Vector3.RIGHT:
+#			return player_grid_position.x < width-1
+#		Vector3.LEFT:
+#			return player_grid_position.x > 0
+#	return false
 
 
 func calculate_facing_points(current_face_value, tile, facing_down_direction) -> int:
