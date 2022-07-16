517b362e416b68c25bc3f3c98d605c644480d68b
Eric Lopes <erichhlopes@gmail.com>
2022-07-16 13:48:29 -0400

    levels

--- src/classes/level.gd
+++ src/classes/level.gd
@@ -4,5 +4,4 @@ class_name Level
 export (Vector2) var LevelSize := Vector2(6,6)
 export (Array) var Tiles: Array
 export (Vector2) var StartPosition: Vector2
-export (Vector2) var Offset: Vector2
-export (bool) var shouldFillGrid = true
+export (bool) var shouldFillGrid = false
