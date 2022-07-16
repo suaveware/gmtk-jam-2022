34915b7a668d8e585f05b2a2a64ca15e11745099
Luiz de Oliveira <luiz@noredink.com>
2022-07-16 14:48:56 -0400

    empty and error textures

--- src/DiceHud/DiceHud.gd
+++ src/DiceHud/DiceHud.gd
@@ -3,13 +3,16 @@ extends GridContainer
 export var player_path: = NodePath()
 
 var dice_textures = [
+	preload("res://src/DiceHud/dieWhite_empty.png"),
 	preload("res://src/DiceHud/dieWhite_border1.png"),
 	preload("res://src/DiceHud/dieWhite_border2.png"),
 	preload("res://src/DiceHud/dieWhite_border3.png"),
 	preload("res://src/DiceHud/dieWhite_border4.png"),
 	preload("res://src/DiceHud/dieWhite_border5.png"),
-	preload("res://src/DiceHud/dieWhite_border6.png")
+	preload("res://src/DiceHud/dieWhite_border6.png"),
 ]
+
+var fail_texture = preload("res://src/DiceHud/dieRed_empty.png")
 
 
 onready var player: Node = get_node(player_path)
@@ -21,7 +24,7 @@ func _process(delta: float) -> void:
 		var image_node: TextureRect = get_node(face + "/" + "FaceImage")
 		var face_value = player.face_values[face]
 
-		if face_value > 0:
-			image_node.texture = dice_textures[face_value - 1]
+		if face_value >= 0:
+			image_node.texture = dice_textures[face_value]
 		else:
-			image_node.texture = null
+			image_node.texture = fail_texture
