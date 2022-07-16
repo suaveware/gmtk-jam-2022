2c188ae41dcc53019e02d18d36974573188f387d
Luiz de Oliveira <luiz@noredink.com>
2022-07-16 10:41:01 -0400

    level editor 2

--- src/LevelEditor/LevelEditor.gd
+++ src/LevelEditor/LevelEditor.gd
@@ -1,33 +1,38 @@
 extends Control
 
-export var size := Vector2(10, 10)
-export var start_position := Vector2(6, 6)
+export var level: Resource = Level.new()
 
 func _ready() -> void:
-	for i in range(size.x):
-		for j in range(size.y):
+	$VBoxContainer/GridContainer.columns = level.LevelSize.x
+	$VBoxContainer/HBoxContainer/LevelName.text = level.resource_name
+	for i in range(level.LevelSize.x):
+		for j in range(level.LevelSize.y):
 			var position := Vector2(i, j)
 			var new_line_edit := LineEdit.new()
 			new_line_edit.align = new_line_edit.ALIGN_CENTER
 			new_line_edit.placeholder_text = position as String
 			new_line_edit.set_meta("position", position)
+
 			$VBoxContainer/GridContainer.add_child(new_line_edit)
+
+	for tile in level.Tiles:
+		var index: int = tile.x * level.LevelSize.x + tile.y
+		var line_edit: LineEdit = $VBoxContainer/GridContainer.get_child(index)
+
+		line_edit.text = tile.z as String
 
 
 func save() -> void:
 	if !$VBoxContainer/HBoxContainer/LevelName.text:
 		return
 
-	var level := Level.new()
-	level.LevelSize = size
-	level.StartPosition = start_position
-
+	level.Tiles = []
 	for line_edit in $VBoxContainer/GridContainer.get_children():
 		if line_edit.text:
 			var position: Vector2 = line_edit.get_meta("position")
 			level.Tiles.push_back(Vector3(position.x, position.y, line_edit.text as int))
 
-	var file_path: String = "res://levels/" + $VBoxContainer/HBoxContainer/LevelName.text + ".tres"
+	var file_path: String = "res://src/levels/" + $VBoxContainer/HBoxContainer/LevelName.text + ".tres"
 	prints("Saving ", file_path)
 	ResourceSaver.save(file_path, level)
 
