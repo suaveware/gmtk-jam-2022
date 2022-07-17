4ef9ceed22883d564b0923029ee96ebb569bcf6a
Luiz de Oliveira <luiz@noredink.com>
2022-07-17 11:02:03 -0400

    keep camera rotation on reload

--- src/autoloads/GlobalState.gd
+++ src/autoloads/GlobalState.gd
@@ -1,3 +1,4 @@
 extends Node
 
-var level_index := 0
+var level_index: = 0
+var camera_rotation: = Vector3.ZERO
--- src/classes/CameraPivot.gd
+++ src/classes/CameraPivot.gd
@@ -4,6 +4,14 @@ signal rotated
 
 var rotation_duration := 1
 
+func _ready() -> void:
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
+	rotation = GlobalState.camera_rotation
+	pass
+
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("rotate_right"):
 		rotate_right()
@@ -14,11 +22,14 @@ func rotate_right() -> void:
 	if $Tween.is_active():
 		return
 
+	var new_rotation_y: = rotation.y + PI / 2
+	GlobalState.camera_rotation.y = new_rotation_y
+
 	$Tween.interpolate_property(
 		self,
 		"rotation:y",
 		rotation.y,
-		rotation.y + PI / 2,
+		new_rotation_y,
 		rotation_duration,
 		$Tween.TRANS_QUAD,
 		$Tween.EASE_IN_OUT
@@ -32,11 +43,14 @@ func rotate_left() -> void:
 	if $Tween.is_active():
 		return
 
+	var new_rotation_y: = rotation.y - PI / 2
+	GlobalState.camera_rotation.y = new_rotation_y
+
 	$Tween.interpolate_property(
 		self,
 		"rotation:y",
 		rotation.y,
-		rotation.y - PI / 2,
+		new_rotation_y,
 		rotation_duration,
 		$Tween.TRANS_QUAD,
 		$Tween.EASE_IN_OUT
@@ -44,5 +58,8 @@ func rotate_left() -> void:
 	$Tween.start()
 	AudioManager.sfx("CameraRotate")
 	emit_signal("rotated", "left")
+
+	yield($Tween, "tween_all_completed")
+	GlobalState.camera_rotation_y = rotation.y
 	pass
 
